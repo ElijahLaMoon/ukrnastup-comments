3d709466d3eeaeb2369a2023eabeb2ef7af1eeb8
Illya <ilya.datsyuk@gmail.com>
2024-01-08 19:32:34 +0200

    since app isnt deployed anywhere yet update old migration in-place

--- comments/src/main/resources/migrations/V001__create_banned_users_table.sql
+++ comments/src/main/resources/migrations/V001__create_banned_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE banned_users (
   telegram_username TEXT,
   reason TEXT,
   banned_by TEXT,
-  banned_by_id INTEGER NOT NULL,
+  banned_by_telegram_id INTEGER NOT NULL,
   message_got_banned_for TEXT,
   message_got_banned_for_link TEXT,
   created_at INTEGER NOT NULL,
