79a3a26defcc38354993959b3cecd107009d8169
Illya <ilya.datsyuk@gmail.com>
2024-01-14 15:57:27 +0200

    update and add new db access methods

--- comments/src/main/scala/org/ukrnastup/comments/Database.scala
+++ comments/src/main/scala/org/ukrnastup/comments/Database.scala
@@ -47,17 +47,43 @@ object Database {
 
   import schema._
   import schema.ctx._
-  val getBannedUsers = processAction(stream(bannedUsers).compile.toList)
-  def insertBannedUser(user: BannedUser) =
-    processAction {
+  // ------------- USERS TABLE METHODS -------------
+  def getBannedUsers(pred: BannedUser => Boolean = _ => true) =
+    processAction(
+      stream(bannedUsers).filter(pred).compile.toList
+    )
+  def insertOrUpdateBannedUser(user: BannedUser) =
+    processAction(
       run(
-        bannedUsers.insertValue(lift(user))
+        bannedUsers
+          .insertValue(lift(user))
+          .onConflictUpdate(_.telegramId)(
+            (t, e) => t.isCurrentlyBanned -> e.isCurrentlyBanned,
+            (t, e) => t.telegramName -> e.telegramName,
+            (t, e) => t.telegramUsername -> e.telegramUsername,
+            (t, e) => t.reason -> e.reason,
+            (t, e) => t.bannedBy -> e.bannedBy,
+            (t, e) => t.bannedByTelegramId -> e.bannedByTelegramId,
+            (t, e) => t.messageGotBannedFor -> e.messageGotBannedFor,
+            (t, e) => t.messageGotBannedForLink -> e.messageGotBannedForLink,
+            (t, e) => t.updatedAt -> e.updatedAt
+          )
       )
-    }
+    )
   def getBannedUserByTelegramId(telegramId: BannedUser.TelegramUserId) =
     processAction(
       run(
         bannedUsers.filter(_.telegramId == lift(telegramId))
+      )
+    )
+  def getBannedUserByUsername(username: BannedUser.TelegramUsername) =
+    processAction(
+      run(
+        bannedUsers
+          .filter(u =>
+            u.telegramUsername.isDefined && // guarantees that username is not null
+              u.telegramUsername.getOrNull == lift(username)
+          )
       )
     )
   def deleteBannedUser(telegramId: BannedUser.TelegramUserId) =
@@ -72,6 +98,51 @@ object Database {
         bannedUsers
           .filter(_.telegramId == lift(user.telegramId))
           .updateValue(lift(user))
+      )
+    )
+  // ------------- ADMINS TABLE METHODS -------------
+  val getAdmins =
+    processAction(
+      stream(admins).compile.toList
+    )
+  def insertAdmin(admin: Admin) =
+    processAction(
+      run(
+        admins.insertValue(lift(admin))
+      )
+    )
+  def insertAdmins(adminsList: List[Admin]) =
+    processAction(
+      run(
+        liftQuery(adminsList).foreach(admins.insertValue(_))
+      )
+    )
+  def getAdminByTelegramId(telegramId: Admin.TelegramUserId) =
+    processAction(
+      run(
+        admins.filter(_.telegramId == lift(telegramId))
+      )
+    )
+  def deleteAdmin(telegramId: Admin.TelegramUserId) =
+    processAction(
+      run(
+        admins.filter(_.telegramId == lift(telegramId)).delete
+      )
+    )
+  def deleteAdmins(adminsList: List[Admin]) =
+    processAction(
+      run(
+        liftQuery(adminsList).foreach(adminToDelete =>
+          admins.filter(_.telegramId == adminToDelete.telegramId).delete
+        )
+      )
+    )
+  def updateAdmin(admin: Admin) =
+    processAction(
+      run(
+        admins
+          .filter(_.telegramId == lift(admin.telegramId))
+          .updateValue(lift(admin))
       )
     )
 }
