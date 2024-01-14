efd49f47f7a25717a21e78b0b5bd55789f2b1fd7
Illya <ilya.datsyuk@gmail.com>
2024-01-14 15:51:30 +0200

    add Admin model

---
+++ comments/src/main/scala/org/ukrnastup/comments/Admin.scala
@@ -0,0 +1,20 @@
+package org.ukrnastup.comments
+
+import java.time.ZonedDateTime
+
+import Admin._
+
+final case class Admin(
+    id: InnerId,
+    telegramId: TelegramUserId,
+    telegramName: TelegramName,
+    telegramUsername: Option[TelegramUsername],
+    createdAt: ZonedDateTime = now()
+)
+
+object Admin {
+  final case class InnerId(value: Long) extends AnyVal
+  final case class TelegramUserId(id: Long) extends AnyVal
+  final case class TelegramName(name: String) extends AnyVal
+  final case class TelegramUsername(username: String) extends AnyVal
+}
